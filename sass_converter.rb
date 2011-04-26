a9d7f679a47bc2ecbbf083468d94f4170ad66c8f
Tate Johnson <tate@tatey.com>
2011-04-26 13:54:56 +1000

    Sass isn't coupled with Haml anymore. Guess syntax based on content, otherwise we can't mix and match sass/scss

--- sass_converter.rb
+++ sass_converter.rb
@@ -2,19 +2,18 @@ module Jekyll
   # Compiled Sass into CSS. You must specify an empty YAML front matter
   # at the beginning of the file.
   # sass|sccs -> .css
-  class SassConverter < Converter
+  class SassConverter < Converter    
     def setup
       return if @setup
       require 'sass'
       @setup = true
     rescue LoadError
       STDERR.puts 'You are missing a library required for sass. Please run:'
-      STDERR.puts '  $ [sudo] gem install haml'
-      raise FatalException.new("Missing dependency: haml")
+      STDERR.puts '  $ [sudo] gem install sass'
+      raise FatalException.new("Missing dependency: sass")
     end
 
     def matches(ext)
-      @ext = ext
       ext =~ /sass|scss/i
     end
 
@@ -24,12 +23,17 @@ module Jekyll
 
     def convert(content)
       setup
-      syntax = (@ext =~ /sass/i) ? :sass : :scss
       begin
-        Sass::Engine.new(content, :style => :compressed, :syntax => syntax).render
+        Sass::Engine.new(content, :style => :compressed, :syntax => syntax(content)).render
       rescue => e
         puts "Sass Exception (#{e.sass_line}): #{e.message}"
       end
+    end
+    
+  private
+    
+    def syntax(content)
+      content.include?(';') ? :scss : :sass
     end
   end
 end
